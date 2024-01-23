b2fde2a0eec4f4da3da5ac82bfbe0140e23a19af
sagar999911 <sagargupta999911@gmail.com>
2024-01-23 16:24:22 +0530

    size and files changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-  # locofy-react-project
+  # Assignment2-figma
 
   Note: Please ensure you have installed <code><a href="https://nodejs.org/en/download/">nodejs</a></code>
 
@@ -7,4 +7,3 @@
   1) Open project folder in <a href="https://code.visualstudio.com/download">Visual Studio Code</a>
   2) In the terminal, run `npm install`
   3) Run `npm start` to view project in browser
-  
--- src/components/SkillsTrending.tsx
+++ src/components/SkillsTrending.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 const SkillsTrending: FunctionComponent = () => {
   return (
     <div className="flex-1 flex flex-col items-start justify-start py-0 pr-[11px] pl-0 box-border min-w-[219px] max-w-full text-sm">
-      <div className="self-stretch rounded-3xs bg-colors-white-100 flex flex-col items-center justify-start p-[30px] box-border gap-[17px] max-w-full mq450:pt-5 mq450:pb-5 mq450:box-border">
+      <div className="self-stretch rounded-3xs bg-colors-white-100 flex flex-col items-center justify-start p-[30px] box-border gap-[6px] max-w-full mq450:pt-5 mq450:pb-5 mq450:box-border">
         <div className="w-[326px] relative rounded-3xs bg-colors-white-100 h-[360px] hidden max-w-full" />
         <div className="self-stretch flex flex-col items-start justify-start gap-[6px] text-3xl">
           <div className="self-stretch flex flex-row items-center justify-between gap-[20px]">
@@ -17,13 +17,13 @@ const SkillsTrending: FunctionComponent = () => {
               src="/icons--more--horizontal@2x.png"
             />
           </div>
-          <div className="w-[253px] relative text-sm leading-[20px] inline-block opacity-[0.4] mix-blend-normal z-[1]">
+          <div className="w-[200px] relative text-sm leading-[20px] inline-block opacity-[0.4] mix-blend-normal z-[1]">
             This will help job seekers to improve on daily basis
             according to trend.
           </div>
         </div>
         <div className="self-stretch h-[41px] flex flex-col items-start justify-start gap-[6px]">
-          <div className="w-[237px] flex flex-row items-start justify-between gap-[20px]">
+          <div className="w-[200px] flex flex-row items-start justify-between gap-[20px]">
             <div className="relative leading-[20px] z-[1]">
               Angular
             </div>
@@ -46,7 +46,7 @@ const SkillsTrending: FunctionComponent = () => {
           </div>
         </div>
         <div className="self-stretch h-[41px] flex flex-col items-start justify-start gap-[6px]">
-          <div className="w-[243px] flex flex-row items-start justify-between gap-[20px]">
+          <div className="w-[200px] flex flex-row items-start justify-between gap-[20px]">
             <div className="relative leading-[20px] z-[1]">Java</div>
             <div className="relative leading-[20px] text-mediumblue text-right z-[1]">
               12,109
@@ -67,13 +67,13 @@ const SkillsTrending: FunctionComponent = () => {
           </div>
         </div>
         <div className="self-stretch h-[41px] flex flex-col items-start justify-start gap-[6px]">
-          <div className="w-[237px] flex flex-row items-start justify-between gap-[20px]">
+          <div className="w-[200px] flex flex-row items-start justify-between gap-[20px]">
             <div className="relative leading-[20px] z-[1]">PHP</div>
             <div className="relative leading-[20px] text-colors-action-info text-right z-[1]">
               132,645
             </div>
           </div>
-          <div className="flex flex-row items-center justify-start py-0 pr-9 pl-0 relative">
+          <div className="flex flex-row items-center justify-start py-0 pr-2 pl-0 relative">
             <img
               className="h-full w-full absolute my-0 mx-[!important] top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full z-[1]"
               alt=""
@@ -88,7 +88,7 @@ const SkillsTrending: FunctionComponent = () => {
           </div>
         </div>
         <div className="self-stretch flex flex-col items-start justify-start gap-[6px]">
-          <div className="w-[239px] flex flex-row items-start justify-between gap-[20px]">
+          <div className="w-[200px] flex flex-row items-start justify-between gap-[20px]">
             <div className="relative leading-[20px] z-[1]">. Net</div>
             <div className="relative leading-[20px] text-orangered text-right z-[1]">
               100,429
